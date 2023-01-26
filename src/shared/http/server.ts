ff169f619d2b5aecff73c2b6f6cacf1aac6bbdc4
Santiago Fleitas <sansfleitas@gmail.com>
2023-01-26 19:50:17 -0300

    Create routes and add a ExpressJS Router

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -2,15 +2,14 @@ import 'dotenv/config';
 import express from 'express';
 import 'express-async-errors';
 import cors from 'cors';
+import { router } from './routes/index';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Olá Dev!' });
-});
+app.use(router);
 
 app.listen(process.env.PORT, () =>
   console.log(`🔥 Server is running on port ${process.env.PORT}!`),
