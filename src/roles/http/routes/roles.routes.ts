4ebd6db93e2195f7c782573defb9f38a8cc0a2d4
Santiago Fleitas <sansfleitas@gmail.com>
2023-01-27 17:24:56 -0300

    Create findAll method to RolesRepository

--- src/roles/http/routes/roles.routes.ts
+++ src/roles/http/routes/roles.routes.ts
@@ -4,6 +4,12 @@ import { RolesRepository } from '@roles/repositories/RolesRepository';
 const rolesRouter = Router();
 const repository = new RolesRepository();
 
+rolesRouter.get('/', (request, response) => {
+  const roles = repository.findAll();
+
+  return response.json(roles);
+});
+
 rolesRouter.post('/', (request, response) => {
   const { name } = request.body;
   const role = repository.create({ name });
