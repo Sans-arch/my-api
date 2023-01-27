4f9bcbfb18a2fdb8e3f48019cbf5a7e961dd4cb3
Santiago Fleitas <sansfleitas@gmail.com>
2023-01-27 17:54:34 -0300

    Create findByName method and add validation in create method

--- src/roles/http/routes/roles.routes.ts
+++ src/roles/http/routes/roles.routes.ts
@@ -10,8 +10,25 @@ rolesRouter.get('/', (request, response) => {
   return response.json(roles);
 });
 
+rolesRouter.get('/:id', (request, response) => {
+  const { id } = request.params;
+  const role = repository.findByName(id);
+
+  if (!role) {
+    return response.status(404).json({ error: 'Role not found!' });
+  }
+
+  return response.json(role);
+});
+
 rolesRouter.post('/', (request, response) => {
   const { name } = request.body;
+  const roleAlreadyExists = repository.findByName(name);
+
+  if (roleAlreadyExists) {
+    return response.status(400).json({ error: 'Role already exists!' });
+  }
+
   const role = repository.create({ name });
 
   return response.status(201).json(role);
