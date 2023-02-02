cf3c16373200a38cf9170a93c513af5c083f36eb
Santiago Fleitas <sansfleitas@gmail.com>
2023-02-02 18:53:08 -0300

    Create Migration to Create Roles Table

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,8 +1,9 @@
 import { DataSource } from 'typeorm';
+import { CreateRolesTable1675374241711 } from './migrations/1675374241711-CreateRolesTable';
 
 export const sqliteDataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
   entities: [],
-  migrations: [],
+  migrations: [CreateRolesTable1675374241711],
 });
---
+++ src/shared/typeorm/migrations/1675374241711-CreateRolesTable.ts
@@ -0,0 +1,32 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateRolesTable1675374241711 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'roles',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuuid',
+            isPrimary: true,
+          },
+          {
+            name: 'name',
+            type: 'string',
+            isUnique: true,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('roles');
+  }
+}
